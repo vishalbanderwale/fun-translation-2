1d67dd8d386d7e87f5d91576b7153c24348bd4b7
vishalbanderwale <vishalsonu945@gmail.com>
2022-12-21 02:23:09 +0530

    updated index,html file and added server url

---
+++ app.js
@@ -0,0 +1,4 @@
+var btnTranslate=document.querySelector("#btn-translate");
+var txtInput=document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output");
+var serverURL="https://api.funtraslation.com/translate/mandalorian.json"
