bbebbeb4ac427e41125b9eb4b625ecae00c66a3a
vishalbanderwale <vishalsonu945@gmail.com>
2022-12-21 03:16:58 +0530

    added css to index.hml

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var btnTranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
 var outputDiv=document.querySelector("#output");
-var serverURL="https://api.funtraslation.com/translate/mandalorian.json"
+var serverURL="https://api.funtranslations.com/translate/mandalorian.json"
 function getTranslationURL(text)
 {
     return serverURL + "?" + "text=" + text 
@@ -11,10 +11,11 @@ function errorHandler(error)
     console.log("Error occured",error);
 alert("something is wrong with server ,try again after sometime");
 }
-var inputText=txtInput.ariaValueMax;
+function clickHandler(){
+var inputText=txtInput.Value;
 fetch(getTranslationURL(inputText))
-.then(response ==> response.json())
-.then(json ==> {
+.then(response => response.json())
+.then(json => {
     var translatedText=json.contents.translated;
     outputDiv.innerText=translatedText;
 })
